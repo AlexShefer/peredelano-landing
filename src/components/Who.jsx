8b03dfab15b1c8bc22b08e506ee113774125bd6b
Aleksandr <Avshef451987@gmail.com>
2023-07-26 13:27:00 +0500

    feat: add Who component~

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import "./Who.css";
 
 function Who() {
+    const images = [
+        "../assets/1.webp",
+        "../assets/2.webp",
+        "../assets/11.webp",
+    ];
     return (
-        <div id="who">
-            <h1>Who section</h1>
-        </div>
+        <section id="who" className="slides">
+            <div className="container">
+                <h2 className="slides__title">
+                    <span className="slides__title_pink"> Мы</span> — в первую
+                    очередь{" "}
+                    <span className="slides__title_pink">сообщество</span>
+                </h2>
+                <div className="row">
+                    <p className="slides__text">
+                        Мы существуем для того, чтобы у вас на новом месте
+                        появились знакомые, друзья и коллеги. Чтобы никто не был
+                        один. Наши конфы проходят в формате уютной домашней
+                        вечеринки.
+                    </p>
+                    <p className="slides__text">
+                        Специально для того, чтобы людям было проще сойтись и
+                        подружиться.После самого ивента сообщество продолжает
+                        жить в чате, бесчисленных встречах, сборах и где угодно
+                        ещё.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
 
