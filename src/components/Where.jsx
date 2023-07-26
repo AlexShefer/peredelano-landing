c31db3cd82261b1b845a241b1b901b74efa310f2
Aleksandr <Avshef451987@gmail.com>
2023-07-26 10:35:12 +0500

    fix: Resolve navigation issue

--- src/components/Where.jsx
+++ src/components/Where.jsx
@@ -1,13 +1,5 @@
-import { useEffect, useState } from "react";
-
 function Where() {
-    const [events, setEvents] = useState([]);
-
-    useEffect(() => {
-        fetch("/api/events") // Send a GET request to the MirageJS server
-            .then((response) => response.json())
-            .then((data) => setEvents(data.events));
-    }, []);
+    // Send a GET request to the MirageJS server /api/events
 
     return (
         <div id="where">
