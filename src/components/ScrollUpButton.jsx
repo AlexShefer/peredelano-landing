68c303719b8aba7b615b4f04100ed2f94c2c8533
Aleksandr <Avshef451987@gmail.com>
2023-07-27 15:09:30 +0500

    feat: Add ScrollUpButton component and fix Navbar

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import "./ScrollUpButton.css";
 
 function ScrollUpButton() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Check the scroll position, for example, when the user scrolls down 100px, show the button
+            if (window.scrollY > 100) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        // Add the scroll event listener when the component mounts
+        window.addEventListener("scroll", handleScroll);
+
+        // Clean up the scroll event listener when the component unmounts
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <>
-            <Link
-                to="navbar"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-                className="scroll-link"
-            >
-                <BsFillArrowUpCircleFill className="scroll-btn" />
-            </Link>
+            {isVisible && (
+                <Link
+                    to="navbar"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="scroll-link"
+                >
+                    <BsFillArrowUpCircleFill className="scroll-btn" />
+                </Link>
+            )}
         </>
     );
 }
